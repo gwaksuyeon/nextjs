4b811e3b6f3cffcd7be76489628fe01fec9c4e5e
DESKTOP-7NKK714\AMD <tndus5837@gmail.com>
2021-09-16 17:36:10 +0900

    젠킨스 테스트

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import dynamic from "next/dynamic";
 const Layout = dynamic(() => import("components/common/layout"));
 
 const Main: React.FC = () => {
-  return <Layout>메인</Layout>;
+  return <Layout>메인 테스트</Layout>;
 };
 
 export default Main;
