5877f5a4db167f0584335942f2eb65e3aec17336
DESKTOP-7NKK714\AMD <tndus5837@gmail.com>
2021-09-28 14:01:50 +0900

    test

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+const withOptimizedImages = require("next-optimized-images");
+
 module.exports = {
   reactStrictMode: true,
   trailingSlash: true,
@@ -6,3 +8,7 @@ module.exports = {
     domains: ["i.esdrop.com", "localhost"],
   },
 };
+
+module.exports = withOptimizedImages({
+  handleImages: ["jpeg", "jpg", "png", "svg"],
+});
