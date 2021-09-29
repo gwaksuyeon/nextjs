c0be66b0af6057d5ae42e9d50ab083e3e2334a22
DESKTOP-7NKK714\AMD <tndus5837@gmail.com>
2021-09-29 16:25:23 +0900

    add library

--- next.config.js
+++ next.config.js
@@ -1,14 +1,5 @@
 /** @type {import('next').NextConfig} */
-const withOptimizedImages = require("next-optimized-images");
-
 module.exports = {
   reactStrictMode: true,
   trailingSlash: true,
-  images: {
-    domains: ["i.esdrop.com", "localhost"],
-  },
 };
-
-module.exports = withOptimizedImages({
-  handleImages: ["jpeg", "jpg", "png", "svg"],
-});
